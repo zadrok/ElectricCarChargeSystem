0e0ca711d1661f16710a57e7a53621eebfcac2a2
Chris-SG <chris.shandgost@gmail.com>
2018-09-04 18:22:27 +1000

    Add charging.

--- src/model/Car.java
+++ src/model/Car.java
@@ -1,6 +1,7 @@
 package model;
 
 import jade.core.Agent;
+import java.lang.Thread;
 
 @SuppressWarnings("serial")
 public class Car extends Agent
@@ -8,6 +9,8 @@ public class Car extends Agent
 	private long id;
 	private long maxChargeCapacity;
 	private long currentCharge;
+	
+	private ChargeThread chargeThread;
 	
 	public Car()
 	{
@@ -54,6 +57,73 @@ public class Car extends Agent
 	
 	public void setCurrentCharge(long aCurrentcharge)
 	{
-		currentCharge = aCurrentcharge;
+		currentCharge = (getMaxChargeCapacity() > aCurrentcharge ? aCurrentcharge : getMaxChargeCapacity());
+	}
+	
+	public void beginCharge(long aChargeRate)
+	{
+		// If charge thread is non-null, already exists
+		// Charge rate must be positive. Consider unsigned?
+		if(!(chargeThread == null) || aChargeRate < 0)
+			return;
+		// Create charge thread using given rate and default of 1000ms rate
+		chargeThread = new ChargeThread(aChargeRate, 1000, this);
+		// Run thread
+		chargeThread.run();
+	}
+	
+	public void stopCharge()
+	{
+		// If charge thread is null, doesn't exist
+		if(chargeThread == null)
+			return;
+		// Call stop
+		chargeThread.stop();
+		// Make thread null
+		chargeThread = null;
+	}
+	
+	// ChargeThread to handle charging of a car
+	public class ChargeThread implements Runnable
+	{
+		// Rate of charge
+		private long chargeRate;
+		// Frequency of charge
+		private long chargePeriod;
+		// Parent car
+		private Car car;
+		// Stop
+		private boolean stop = false;
+		
+		public ChargeThread(long aChargeRate, long aChargePeriod, Car aCar)
+		{
+			// Set member variables
+			chargeRate = aChargeRate;
+			chargePeriod = aChargePeriod;
+			car = aCar;
+		}
+		
+		public void stop()
+		{
+			// Stop running
+			stop = true;
+		}
+		
+		public void run()
+		{
+			while(!stop)
+			{
+				// Sleep for given period of time
+				try {
+					Thread.sleep(chargePeriod);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				// Increment car charge
+				car.setCurrentCharge(car.getCurrentCharge() + chargeRate);
+				System.out.println( "Car " + car.getID() + " has charge " + car.getCurrentCharge() );
+			}
+		}
 	}
 }
