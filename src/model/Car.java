4f90672aff519ef1d8f5ef35813cae02c9dfac36
lachlan (zadrok) <lachlan.e.haggart@gmail.com>
2018-09-19 15:26:23 +1000

    opps

--- src/model/Car.java
+++ src/model/Car.java
@@ -8,104 +8,100 @@ import java.util.concurrent.atomic.AtomicBoolean;
 public class Car extends Agent
 {
 	public static enum STATE { NONE, IDEL, CHARGE };
-	
+
 	private long id;
 	private long maxChargeCapacity;
 	private long currentCharge;
 
 	// looked at by gui
 	private STATE carState;
-	
+
 	// used in gui
 	private double startAngle;
-	
+
 	public Car()
 	{
 		super();
 		id = -1;
 		maxChargeCapacity = 1000;
 		currentCharge = 0;
-		
+
 		initCommon();
 	}
-	
+
 	public Car(long aID, long aMaxChargeCapacity, long aCurrentCharge)
 	{
 		super();
 		id = aID;
 		maxChargeCapacity = aMaxChargeCapacity;
 		currentCharge = aCurrentCharge;
-		
+
 		initCommon();
 	}
-	
+
 	private void initCommon()
 	{
 		carState = STATE.IDEL;
 		startAngle = 0;
 	}
-	
+
 	protected void setup()
-	{	
+	{
 		addBehaviour( new CarBehaviourBasic( this ) );
 	}
-	
+
 	public long getID()
 	{
 		return id;
 	}
-	
+
 	public void setID(long aID)
 	{
 		id = aID;
 	}
-	
+
 	public long getMaxChargeCapacity()
 	{
 		return maxChargeCapacity;
 	}
-	
+
 	public void setMaxChargeCapacity(long aMaxchargeCapacity)
 	{
 		maxChargeCapacity = aMaxchargeCapacity;
 	}
-	
+
 	public long getCurrentCharge()
 	{
 		return currentCharge;
 	}
-	
+
 	public void setCurrentCharge(long aCurrentcharge)
 	{
 		currentCharge = (getMaxChargeCapacity() > aCurrentcharge ? aCurrentcharge : getMaxChargeCapacity());
 	}
-	
+
 	public double getStartAngle()
 	{
 		return startAngle;
 	}
-	
+
 	public void setStartAngle(double aAngle)
 	{
 		startAngle = aAngle;
 	}
-	
+
 	public STATE getCarState()
 	{
 		return carState;
 	}
-	
-<<<<<<< HEAD
+
+
 	public String getCarStateString()
 	{
 		return getCarState().toString();
 	}
 	
-	// ChargeThread to handle charging of a car
-	public class ChargeThread implements Runnable
-=======
 	public void setCarState(STATE aState)
->>>>>>> 1344da17aa069bcec0fcea9bc0fd14ae338abc1a
 	{
 		carState = aState;
 	}
